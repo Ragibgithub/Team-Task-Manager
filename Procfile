6c12e3bd754a2f1156512fdb8d4f374362e4b8ca
Ragib Ahmad <ragibahmadsrd786@gmail.com>
2026-05-03 10:43:09 +0530

    final fix: procfile path + settings debug

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 
-web: python manage.py migrate && gunicorn taskmanager.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn taskmanager.taskmanager.wsgi:application --bind 0.0.0.0:$PORT
 
--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y#9r5ydm%r=(y9!ty17zf63suj)6@4p2-#f_pyetjqq@!n+mzz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
